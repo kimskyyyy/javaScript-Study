ab0a36cf7dd50fd50807a223f79719d5af9b318b
kimskyyyy <hhhhh823@naver.com>
2023-05-28 21:42:00 +0900

    주석 추가

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+// 1. strict mode(엄격 모드)
+// JavaScript는 매우 유연함 --> 유연한 것은 위험함(개발자가 실수할 수 있음)
+// ECMAScript5부터 엄격모드가 더해졌음
+// 'use strict'를 선언하면 비상식적인 문법을 잡아줌(정의되지 않은 변수 사용 등)
+// 파일 상단에 기재해서 사용함
+'use strict';
 console.log('Hello World!');
